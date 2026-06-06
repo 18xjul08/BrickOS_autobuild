c1811ddd243d1eec22b5a24a42beb4c5a989d0da
Mẫn Văn Công <mvc.manvancong18072008@gmail.com>
2026-06-06 10:59:47 +0700

    Add Leica bootanimation
    Add Leica bootanimation for Xiaomi 13,14,15 series

---
+++ bin/modfile/OS3/bootani/update.sh
@@ -0,0 +1,11 @@
+WORK_DIR=$(pwd)
+codename=$(cat $WORK_DIR/bin/ddevice/device_code.txt)
+
+
+BootAniDIR=$WORK_DIR/build/baserom/images/product/media
+
+
+if [[ $codename == "dada" || $codename == "haotian" || $codename == "xuanyuan" || $codename == "houji" || $codename == "shennong" || $codename == "aurora" || $codename == "fuxi" || $codename == "nuwa" || $codename == "ishtar" ]];then
+    rm -rf $BootAniDIR/bootanimation.zip
+    cp -rf $WORK_DIR/bin/modfile/OS3/bootani/bootanimation.zip $BootAniDIR
+    mods "Added LEICA bootanimation Done"
