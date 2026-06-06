ea5910fab90c82a94d4268487715bd9a81383a77
Mẫn Văn Công <mvc.manvancong18072008@gmail.com>
2026-06-06 11:59:45 +0700

    Remove LEICA bootanimation log message

--- bin/modfile/OS3/bootani/update.sh
+++ bin/modfile/OS3/bootani/update.sh
@@ -8,5 +8,4 @@ BootAniDIR=$WORK_DIR/build/baserom/images/product/media
 if [[ $codename == "dada" || $codename == "haotian" || $codename == "xuanyuan" || $codename == "houji" || $codename == "shennong" || $codename == "aurora" || $codename == "fuxi" || $codename == "nuwa" || $codename == "ishtar" ]];then
     rm -rf $BootAniDIR/bootanimation.zip
     cp -rf $WORK_DIR/bin/modfile/OS3/bootani/bootanimation.zip $BootAniDIR
-    mods "Added LEICA bootanimation Done"
 fi
