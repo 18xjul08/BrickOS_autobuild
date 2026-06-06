d8a2e117ee4a3c07d430b2a1c568478219ea0be7
Mẫn Văn Công <mvc.manvancong18072008@gmail.com>
2026-06-06 14:13:54 +0700

    Update bootani script to manage bootanimation files

--- bin/modfile/OS3/bootani/update.sh
+++ bin/modfile/OS3/bootani/update.sh
@@ -6,8 +6,6 @@ BootAniDIR=$WORK_DIR/build/baserom/images/product/media
 
 
 if [[ $codename == "DADA" || $codename == "HAOTIAN" || $codename == "XUANYUAN" || $codename == "HOUJI" || $codename == "SHENNONG" || $codename == "AURORA" || $codename == "FUXI" || $codename == "NUWA" || $codename == "ISHTAR" ]];then
-mods "Add Leica bootanimation for flagship devices"
 rm -rf $BootAniDIR/bootanimation.zip
 cp -rf $WORK_DIR/bin/modfile/OS3/bootani/bootanimation.zip $BootAniDIR
-mods "Add Leica bootanimation done"
 fi
